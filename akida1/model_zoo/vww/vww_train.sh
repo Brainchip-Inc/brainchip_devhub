557bb02d250203f4c0d6ad3065906795cd21ab40
Douglas McLelland <dmclelland@brainchipinc.com>
2026-06-10 22:33:20 +0200

    bash script to run full pipeline

---
+++ akida1/model_zoo/vww/vww_train.sh
@@ -0,0 +1,16 @@
+DATADIR="${1:-}"
+DATA_ARG=${DATADIR:+-d "$DATADIR"}
+
+python vww_model.py -s models/akidanet_vww_untrained.h5
+
+python vww_train.py -l models/akidanet_vww_untrained.h5 -s models/akidanet_vww.h5 -e 50 -lr 1e-3 $DATA_ARG
+python vww_eval.py -l models/akidanet_vww.h5 $DATA_ARG
+
+# 4 bits quantization and tuning
+cnn2snn quantize -m models/akidanet_vww.h5 -i 8 -w 4 -a 4
+python vww_train.py -l models/akidanet_vww_iq8_wq4_aq4.h5 -s models/akidanet_vww_qat.h5 -lr 1e-4 -e 2 $DATA_ARG
+
+python vww_eval.py -l models/akidanet_vww_qat.h5 $DATA_ARG
+
+cnn2snn convert -m models/akidanet_vww_qat.h5
+python vww_eval.py -l models/akidanet_vww_qat.fbz $DATA_ARG
